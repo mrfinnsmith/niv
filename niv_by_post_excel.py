46f6a716efed568b69799b66696ce18d8a97996d
Finn Smith <finnbarsmith@gmail.com>
2024-08-18 20:49:03 -0700

    Check that headers are correct on spreadsheet

--- niv_by_post_excel.py
+++ niv_by_post_excel.py
@@ -19,15 +19,13 @@ def log_all_links(url):
 
     for link in links:
         href = link.get('href')
-        if href and 'pdf' in href:  # Check if 'href' attribute exists and contains 'pdf'
-            print(link.text)  # Print the text of the link
+        if href and 'pdf' in href and 'by post' in link.text.lower():  # Check if 'href' attribute exists, contains 'pdf', and 'by post' in the text
             
             # Extracting the date from the link text
             match = re.search(r'\b(\w+)\s+(\d{4})\b', link.text)
             if match:
                 month, year = match.groups()
                 date_value = datetime.datetime.strptime(f'{month} {year}', '%B %Y')
-                print(date_value)  # Print the date value
                 
                 # Update latest date if found date is more recent
                 if date_value > latest_date:
@@ -41,11 +39,21 @@ def log_all_links(url):
     if latest_date > datetime.datetime.min:
         print("Latest date found:", latest_date)
         print("Here is the latest Excel link:", latest_excel_link)
+        df = pd.read_excel(latest_excel_link, sheet_name='Sheet1', skiprows=1)
+
+        # Normalize column names
+        df.columns = df.columns.str.upper().str.strip().str.replace("'", "").str.replace(" ", "_")
+
+        print("\n".join(df.columns.tolist()))
+
+        # Check if required columns are present
+        required_columns = {'POST', 'VISA_CLASS', 'ISSUANCES'}
+        if not required_columns.issubset(set(df.columns)):
+            print("Required columns are missing.")
+            return
         
-        # Read and display the first 10 lines from the Excel file
-        if latest_excel_link:
-            df = pd.read_excel(latest_excel_link, sheet_name='Sheet1')
-            print(df.head(10))
+        # Proceed with data processing if columns are found
+        print(df.head())
 
 # URL to be passed to the function
 url = "https://travel.state.gov/content/travel/en/legal/visa-law0/visa-statistics/nonimmigrant-visa-statistics/monthly-nonimmigrant-visa-issuances.html"
